57c1ccdc9da42ebf895760470ec1cba16e5d9219
unknown <yousalb891@student.polsl.pl>
2023-03-23 23:31:49 +0100

    playing around

--- healthhub/components/layout.js
+++ healthhub/components/layout.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import hospitalIcon from '../public/hospital.png'
+
 
 export default function Layout({ children }) {
   const router = useRouter();
@@ -21,24 +23,20 @@ export default function Layout({ children }) {
 
   return (
     <div className='min-h-screen flex flex-col'>
-      <header className='bg-purple-200 sticky top-0 h-14 flex justify-center items-center font-semibold uppercase'>
-        Next.js sidebar menu
+      <header className='bg-purple-200 sticky top-0 flex h-8 justify-center items-center font-semibold uppercase'>
+      <img src={hospitalIcon} />
       </header>
       <div className='flex flex-col md:flex-row flex-1'>
-        <aside className='bg-fuchsia-100 w-full md:w-60'>
+        <aside className='bg-gray-200 w-full md:w-60'>
           <nav>
             <ul>
               {menuItems.map(({ href, title }) => (
                 <li className='m-2' key={title}>
                   <Link href={href} passHref>
-                    <div>
-                    <a
-                      className={`flex p-2 bg-fuchsia-200 rounded hover:bg-fuchsia-400 cursor-pointer ${
-                        router.asPath === href && 'bg-fuchsia-600 text-white'
-                      }`}
-                    >
-                      {title}
-                    </a>
+                <div className={`flex p-2 bg-white hover:bg-gray-400 cursor-pointer rounded-xl ${
+                        router.asPath === href && 'bg-indigo-200'
+                      }`}>
+                    <span className="font-semibold text-blue-600">{title}</span>
                     </div>
                   </Link>
                 </li>
