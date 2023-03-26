95dd44c6d5a0c0bbde69652d05c579e28516543e
claude-perrin <vity5.diduk@gmail.com>
2023-03-26 19:01:22 +0200

    define path for modules

--- healthhub/src/pages/index.tsx
+++ healthhub/src/pages/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import type { GetServerSideProps } from "next";
-import Layout from "../components/Layout";
-import Post, { PostProps } from "../components/Post";
-import prisma from '../lib/prisma'
+import Layout from "../../components/Layout";
+import Post, { PostProps } from "../../components/Post";
+import prisma from '../../lib/prisma'
 
 
 function index() {
