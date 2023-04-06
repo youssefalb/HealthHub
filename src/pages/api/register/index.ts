0aab5c985b2def93c44b6d6cba99d5646b29ef95
unknown <yousalb891@student.polsl.pl>
2023-04-06 14:10:48 +0200

    Register sending email and verfication done

--- src/pages/api/register/index.ts
+++ src/pages/api/register/index.ts
@@ -1,31 +1,38 @@
 import prisma from '../../../../lib/prisma'
 import {hashPassword}  from '../../../utils/hashPassword'
+import sendVerificationEmail from "../sendVerficationEmail";
+
 
 //create a hashing function
 
 export default async function handler(req, res) {
-    const { fname, lname, email, password, insurance_id, national_id } = req.body
-    console.log(email)
+    const userData = req.body
+   // console.log(userData.email)
     const user = await prisma.user.findUnique({
         where: {
-            email: email
+            email: userData.email
         }
     })
 
     
     if (!user) {
-        const hashedPassword = await hashPassword(password)
+ 
+        const hashedPassword = await hashPassword(userData.password)
         console.log(hashedPassword)
-        const result = await prisma.user.create({
+        var result = null
+        result = await prisma.user.create({
             data: {
-                fname: fname,
-                lname: lname,
-                email: email,
+                fname: userData.fname,
+                lname: userData.lname,
+                email: userData.email,
                 password: hashedPassword,
-                nationalID: national_id,
-                patient: { create: { insurance_id: insurance_id } }
+                nationalID: userData.national_id,
+                patient: { create: { insurance_id: userData.insurance_id } }
             }
-        })
+       })
+       console.log('before Verification ${user}')
+       await sendVerificationEmail(result);
+       console.log("After Verification")
        res.status(200).json({ result })
     } else {
         res.status(401).json({ result: null })
---
+++ src/pages/api/sendVerficationEmail.ts
@@ -0,0 +1,43 @@
+import nodemailer from "nodemailer";
+import jwt from "jsonwebtoken";
+
+
+const generateToken = (user) => {
+  const payload = {
+    id: user.id,
+    email: user.email,
+  };
+  const secret = process.env.SECRET;
+  const options = {
+    expiresIn: "1h",
+  };
+  return jwt.sign(payload, secret, options);
+};
+
+// create a Nodemailer transporter
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+export default async function sendVerificationEmail(user) {
+  const verificationLink = `http://localhost:3000/api/verify?token=${generateToken(user)}`;
+  
+    const mailOptions = {
+      from: process.env.SMTP_FROM,
+      to: user.email,
+      subject: "Please verify your email address",
+      html: `<p>Hi ${user.name},</p><p>Please click the following link to verify your email address:</p><a href="${verificationLink}">${verificationLink}</a>`,
+    };
+  
+    try {
+      await transporter.sendMail(mailOptions);
+      console.log(`Verification email sent to ${user.email}`);
+    } catch (error) {
+      console.error(`Error sending verification email to ${user.email}: ${error}`);
+    }
+  }
