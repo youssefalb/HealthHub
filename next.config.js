4c362cd6917baebd4d97ce643d925dd44c690e20
Michał Smaluch <smaluchmichal@gmail.com>
2023-04-07 22:46:28 +0200

    Revert "directory restructure "

--- next.config.js
+++ next.config.js
@@ -1,26 +1,6 @@
-// const { withSuperjson } = require('next-superjson')
+const { withSuperjson } = require('next-superjson')
 
-// module.exports = withSuperjson()({
-//     output: 'standalone',
-// })
+module.exports = withSuperjson()({
+    output: 'standalone',
+})
 
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        appDir: true,
-        serverComponentsExternalPackages: ['bcrypt'],
-        // swcPlugins: [["next-superjson-plugin", {}]],
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    output: "standalone",
-    
-}
-
-module.exports = nextConfig
