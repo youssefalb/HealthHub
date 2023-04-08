420dbe4cfa324add1b817a9d3c82062cda296e8f
“Asser” <asser.elfeki@gmail.com>
2023-04-08 17:16:13 +0200

    sada

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}','./components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-
+      colors: {
+        'primary': '#329AFC',
+      },
       backgroundImage: {
         'hero': "url('../public/images/background.png')",
       },
