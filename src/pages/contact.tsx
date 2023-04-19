417f77ca6535d6ac31a7bf0c71dbac5f5b207165
youssefalb <yousalb891@student.polsl.pl>
2023-04-19 16:50:10 +0200

    place holder replaced

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -58,7 +58,7 @@ function Contact() {
           <h2 className="text-lg font-bold mb-2">{developer.name}</h2>
           <p className="text-gray-500 mb-4">{developer.position}</p>
           <CustomButton
-                buttonText="Custom Text"
+                buttonText="View on GitHub"
                 onClick={() => handleGitHubButtonClick(developer.githubUrl)}
               />
         </div>
