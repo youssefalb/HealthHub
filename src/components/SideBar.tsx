f2e458b969f926c8189b35d8180b231a62b0597d
AsserElfeki <asser.elfeki@gmail.com>
2023-04-21 16:24:00 +0200

    refactor(SideBar.tsx): simplify menuItems creation and reverse order feat(SideBar.tsx): add support for patient messages and tests pages feat(messages.tsx): create messages page for patients feat(tests.tsx): create tests page for patients

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,93 +4,102 @@ import Link from "next/link";
 import { Role } from "@prisma/client";
 
 export default function sideBar() {
+  const { data: session } = useSession();
 
-    const { data: session } = useSession();
+  let menuItems = [
+    {
+      href: "/about",
+      title: "About",
+    },
+    {
+      href: "/contact",
+      title: "Contact",
+    },
+    {
+      href: "/locations",
+      title: "Find Us",
+    },
+  ];
 
-    let menuItems = [
-    ];
-    let path = "/"
+  let path = "/";
 
-    if (session?.user?.role === Role.PATIENT) {
-        path = "/patient"
-        menuItems = [
-            {
-                href: "/visits",
-                title: "Appointments",
-            },
-            {
-                href: path + "/tests",
-                title: "Tests",
-            },
-            {
-                href: path + "/messages",
-                title: "Messages",
-            },
-            {
-                href: "/findUs",
-                title: "Find Us",
-            },
-        ];
-    }
-    else if (session?.user?.role === Role.DOCTOR) {
-        path = "/doctor"
-        menuItems = [
-            {
-                href: "/visits",
-                title: "Appointments",
-            },
+  if (session?.user?.role === Role.PATIENT) {
+    console.log("user is patient");
 
-        ];
-    }
-    else if (session?.user?.role === Role.LAB_ASSISTANT) {
+    path = "/patient";
+    menuItems = menuItems
+      .concat([
+        {
+          href: "/tests",
+          title: "Tests",
+        },
+        {
+          href: "/visits",
+          title: "Appointments",
+        },
+        {
+          href: "/profile/messages",
+          title: "Messages",
+        },
+      ])
+      .reverse();
+    console.log(menuItems);
+  } else if (session?.user?.role === Role.DOCTOR) {
+    path = "/doctor";
+    menuItems = menuItems
+      .concat([
+        {
+          href: "/visits",
+          title: "Appointments",
+        },
+      ])
+      .reverse();
+  } else if (session?.user?.role === Role.LAB_ASSISTANT) {
+  } else if (session?.user?.role === Role.LAB_SUPERVISOR) {
+  } else if (session?.user?.role === Role.RECEPTIONIST) {
+  } else {
+    // menuItems = [
+    //     {
+    //         href: "/about",
+    //         title: "About",
+    //     },
+    //     {
+    //         href: "/contact",
+    //         title: "Contact",
+    //     },
+    //     {
+    //         href: "/locations",
+    //         title: "Find Us",
+    //     },
+    // ];
+  }
 
-    }
-
-    else if (session?.user?.role === Role.LAB_SUPERVISOR) {
-
-    }
-    else if (session?.user?.role === Role.RECEPTIONIST) {
-
-    }
-    else {
-        menuItems = [
-            {
-                href: "/about",
-                title: "About",
-            },
-            {
-                href: "/contact",
-                title: "Contact",
-            },
-        ];
-    }
-
-
-
-    return (
-        <aside className="bg-gray-100 w-full md:w-60">
-            {session ? <div className="flex items-center justify-center  rounded-full border-2 border-black w-3/6">
-                {/* <img src="/images/logotype.png" alt=""></img> */}
-                profile pic here
-            </div> : null}
-            <nav>
-                <ul>
-                    {menuItems.map(({ href, title }) => (
-                        <li className="m-2" key={title}>
-                            <Link href={href} passHref>
-                                <div
-                                    className="flex p-2 bg-gray-50 hover:bg-gray-200 cursor-pointer rounded-lg
+  return (
+    <aside className="bg-gray-100 w-full md:w-60">
+      {session ? (
+        <div className="flex items-center justify-center  rounded-full border-2 border-black w-3/6">
+          {/* <img src="/images/logotype.png" alt=""></img> */}
+          profile pic here
+        </div>
+      ) : null}
+      <nav>
+        <ul>
+          {menuItems.map(({ href, title }) => (
+            <li className="m-2" key={title}>
+              <Link href={href} passHref>
+                <div
+                  className="flex p-2 bg-gray-50 hover:bg-gray-200 cursor-pointer rounded-lg
                      "
-                                >
-                                    <span className="font-semibold text-gray-600 mx-6 text-sm">
-                                        {title}
-                                    </span>
-                                </div>
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </aside>
-    );
+                >
+                  <span className="font-semibold text-gray-600 mx-6 text-sm">
+                    {title}
+                  </span>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
 }
