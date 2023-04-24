0f375e353174f768fca1b68928f38bb1fa35a7e6
Andriy Bobchuk <andriybobchuk@gmail.com>
2023-04-24 12:52:33 +0200

    Google maps integration + frontend

--- src/pages/locations.tsx
+++ src/pages/locations.tsx
@@ -1,13 +1,49 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import hospitalLocations from './hospitalLocations.json';
 
+const HospitalMarker = () => (
+  <div className="flex justify-center items-center w-4 h-4 rounded-full bg-red-600">
+    <div className="w-2 h-2 rounded-full bg-white"></div>
+  </div>
+);
 
-export default function locations() 
-{
-    return (
-        <div>
-            <h1 className='text-4xl font-sans'>My Locations</h1>
-            <div className='flex justify-center items-center border-2 border-black h-screen'>
-                here there will be a list of locations and the google maps component 
-            </div>
-        </div>
-    )
-}
+const HospitalCard = ({ hospital }) => {
+  const { name, city, street, phoneNumber, latitude, longitude } = hospital;
+
+  const center = { lat: latitude, lng: longitude };
+
+  return (
+    <div className="w-full md:w-1/2 lg:w-1/3 p-4 bg-gray-100 rounded-xl p-8 m-4">
+       <div className="mt-4 mb-6 text-gray-800">
+        <p className="text-2xl font-bold">{name}</p>
+      </div>
+      <div className="h-56 md:h-64 lg:h-80">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyBewRNKS1_9MVxEbzvrn2qrhQqVWguLeJk" }}
+          defaultCenter={center}
+          defaultZoom={14}
+        >
+          <HospitalMarker lat={latitude} lng={longitude} />
+        </GoogleMapReact>
+      </div>
+      <div className="mt-4 text-gray-800">
+        <p className="text-lg font-bold">{city}</p>
+        <p className="text-sm">{street}</p>
+        <p className="text-lg font-bold text-blue-500">{phoneNumber}</p>
+      </div>
+    </div>
+  );
+};
+
+const HospitalScreen = () => {
+  return (
+    <div className="flex flex-wrap justify-center items-center h-screen">
+      {hospitalLocations.map((hospital) => (
+        <HospitalCard key={hospital.id} hospital={hospital} />
+      ))}
+    </div>
+  );
+};
+
+export default HospitalScreen;
