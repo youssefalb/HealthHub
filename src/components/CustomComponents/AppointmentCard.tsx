b500d7028d6df7509b94a883058e5c690f1b1165
Andriy Bobchuk <andriybobchuk@gmail.com>
2023-04-25 14:46:41 +0200

    test commit

---
+++ src/components/CustomComponents/AppointmentCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// Used by Patient and Registrar.   
+
+const AppointmentCard = ({ highlight, title, subtitle }) => {
+    return (
+        <div className="bg-white rounded-lg shadow-md p-6 flex items-center justify-between">
+        <div className="flex items-center">
+          <div>
+            <span className="text-blue-500 text-sm block">{highlight}</span>
+            <h2 className="text-lg font-bold">{title}</h2>
+            <span className="text-gray-500 text-sm">{subtitle}</span>
+          </div>
+        </div>
+        <div className="flex items-center">
+          <button className="rounded-full w-10 h-10 flex items-center justify-center mr-2">
+            <img src="/images/info.png" alt="More Info Icon" className="h-11 w-11" />
+          </button>
+          <button className="rounded-full w-10 h-10 flex items-center justify-center">
+            <img src="/images/cancel.png" alt="Cancel Booking Icon" className="h-11 w-11" />
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  export default AppointmentCard;
