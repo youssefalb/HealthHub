a8ec4d135f5c87a9ce93dae18d68d81a2a3207f0
youssefalb <yousalb891@student.polsl.pl>
2023-05-02 21:22:52 +0200

    Examinations EPs

--- src/pages/api/examinations/index.ts
+++ src/pages/api/examinations/index.ts
@@ -3,7 +3,7 @@ import { getServerSession } from "next-auth/next";
 import prisma from "@/lib/prisma";
 import { authOptions } from "../auth/[...nextauth]";
 import { Role, Status } from "@prisma/client";
-
+// Everthing but not cancelled
 interface JSONClause {
     [key: string]: any;
 }
@@ -53,23 +53,44 @@ export default async function handler(
 
     // Patient, Doctor or Registrar viewing visits
     else if (req.method === "GET") {
-        //console.log("not here")
         try {
             let results: string | any[];
-            if (session.user.role == Role.PATIENT  || session.user.role == Role.DOCTOR || session.user.role == Role.RECEPTIONIST) {
-                const {visit_id} = req.query;
-                let whereClause: JSONClause = {};
-                whereClause.visitId = visit_id;
+            if (session.user.role == Role.PATIENT  ) {
                 results = await prisma.physicalExamination.findMany({
-                    where: whereClause,
+                    where:{
+                        visit:{
+                            patientId:session.user.id
+                        }
+                    }
                 });
-            } else {
+            }
+            else if (session.user.role == Role.RECEPTIONIST){
+                results = await prisma.physicalExamination.findMany({
+                });
+            } 
+            else if (session.user.role == Role.DOCTOR){
+
+                results = await prisma.physicalExamination.findMany({
+                    where: {
+                        visit: {
+                            doctorId: session.user.id
+                        }
+                    },
+                });
+                
+            }
+            else {
                 return res.status(401).json({
                     success: false,
                     message: "Only patients, doctors and receptionists can view physical examinations",
                 });
             }
+            if (results){
             return res.status(200).json({ success: true, data: results });
+            }
+            else {
+                return res.status(404).json({ success: false, message: "No physical examinations found" });
+            }
         } catch (error) {
             //here should be a redirect to a general purpose error page
             return res
