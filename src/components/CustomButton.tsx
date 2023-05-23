2291431c012441369e9ea45cab08698c857db920
youssefalb <yousalb891@student.polsl.pl>
2023-05-23 18:43:19 +0200

    some adjustuments in the settings frontend

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const CustomButton = ({buttonText, onClick }) => {
   return (
     <button
-      className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-3xl"
+      className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-3xl mb-8"
       onClick={onClick}
     >
       {buttonText}
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,16 +1,41 @@
+import { useSession } from "next-auth/react";
 import { useState } from "react";
 import TextInput from '@/components/textInput';
 import InsuranceForm from '@/components/InsuranceForm';
 import CustomButton from '@/components/CustomButton';
+import { Role } from '@prisma/client';
+import ProfilePicture from '@/components/ProfilePicture';
+import { useEffect } from "react";
 
 const UserSettings = () => {
+    const { data: session } = useSession(); // it's not fired everytime, (only once), but I need to declare it to be able to access it
+
+    const role = session?.user?.role;
+
+    const [profileImage, setProfileImage] = useState('');
+    useEffect(() => {
+        if (session?.user?.image) {
+            setProfileImage(session.user.image);
+        }
+    }, [session?.user?.image]);
     const [idNumber, setIdNumber] = useState('');
+    const [firstName, setfirstName] = useState('');
+    const [lastName, setlastName] = useState('');
+
+    const [email, setEmail] = useState('');
+
     const [hasInsurance, setHasInsurance] = useState(false);
     const [insuranceNumber, setInsuranceNumber] = useState('');
 
-    const handleSubmit = (e) => {
+
+    const handlePictureClick = (e) => {
         e.preventDefault();
-        // submit handler logic
+        console.log(session?.user?.image);
+    };
+
+    const handleSubmit = (e) => {   
+        e.preventDefault();
+        console.log(role);
     };
 
     return (
@@ -21,9 +46,15 @@ const UserSettings = () => {
                 <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
             </div>
             {/* Profile picture with edit icon */}
-            <div className="relative mx-auto -mt-16 w-32 h-32 rounded-full overflow-hidden border-4 border-white">
-                <img src="https://picsum.photos/200" alt="Profile" className="w-full h-full object-cover" />
-
+            <div
+                className="relative mx-auto -mt-16 w-32 h-32 rounded-full overflow-hidden border-4 border-white cursor-pointer"
+                onClick={handlePictureClick}
+            >
+            <img
+                src={profileImage ? profileImage : "https://avatars.githubusercontent.com/u/72605819?v=4"}
+                alt={session?.user?.name}
+                className="w-full h-full object-cover"
+                />
             </div>
             <div className="mx-auto max-w-screen-lg my-8 px-4">
                 <form onSubmit={handleSubmit}>
@@ -37,23 +68,58 @@ const UserSettings = () => {
                         value={idNumber}
                         onChange={(e) => setIdNumber(e.target.value)}
                     />
+
+                    <CustomButton
+                            buttonText={"Save Changes"}
+                            onClick={() => {
+                                //Change name and username
+                            }}
+                        />
                     <TextInput
-                        label="PESEL Number*"
+                        label="Email*"
                         value={idNumber}
-                        onChange={(e) => setIdNumber(e.target.value)}
-                    />
-                    <InsuranceForm
-                        hasInsurance={hasInsurance}
-                        setHasInsurance={setHasInsurance}
-                        insuranceNumber={insuranceNumber}
-                        setInsuranceNumber={setInsuranceNumber}
+                        onChange={(e) => setEmail(e.target.value)}
                     />
                     <CustomButton
-                        buttonText={"Save Changes"}
-                        onClick={() => {
-                            //do stuff
-                        }}
+                            buttonText={"Save Changes"}
+                            onClick={() => {
+                                //Change name and username
+                            }}
+                        />
+
+                        
+                    {role == Role.PATIENT && (
+                    // <div>
+                    // <InsuranceForm
+                    //     hasInsurance={hasInsurance}
+                    //     setHasInsurance={setHasInsurance}
+                    //     insuranceNumber={insuranceNumber}
+                    //     setInsuranceNumber={setInsuranceNumber}
+                    // />
+
+                    //       <CustomButton
+                    //         buttonText={"Save Changes"}
+                    //         onClick={() => {
+                    //             //Change name and username
+                    //         }}
+                    //     />
+                    // </div>
+                    <div>
+                    <TextInput
+                        label="Innsurance Number*"
+                        value={insuranceNumber}
+                        onChange={(e) => setInsuranceNumber(e.target.value)}
                     />
+                        <CustomButton
+                            buttonText={"Save Changes"}
+                            onClick={() => {
+                                //Change the insurance number
+                            }}
+                        />
+                    </div>
+                    )}
+
+           
                 </form>
             </div>
         </div>
