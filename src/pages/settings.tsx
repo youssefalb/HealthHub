e01a5e341ffb958e444003eafb93168136fccf0c
youssefalb <yousalb891@student.polsl.pl>
2023-05-23 19:13:30 +0200

    Profil pic in settings and pic changing functionnality (Not in the db yet)

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,11 +1,8 @@
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useEffect, useCallback , useState} from "react";
 import TextInput from '@/components/textInput';
-import InsuranceForm from '@/components/InsuranceForm';
 import CustomButton from '@/components/CustomButton';
 import { Role } from '@prisma/client';
-import ProfilePicture from '@/components/ProfilePicture';
-import { useEffect } from "react";
 
 const UserSettings = () => {
     const { data: session } = useSession(); // it's not fired everytime, (only once), but I need to declare it to be able to access it
@@ -18,9 +15,20 @@ const UserSettings = () => {
             setProfileImage(session.user.image);
         }
     }, [session?.user?.image]);
-    const [idNumber, setIdNumber] = useState('');
-    const [firstName, setfirstName] = useState('');
-    const [lastName, setlastName] = useState('');
+
+    const handlePictureChange = useCallback((e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                setProfileImage(event.target.result.toString());
+                console.log(session?.user?.image);
+            };
+            reader.readAsDataURL(file);
+        }
+    }, []);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
 
     const [email, setEmail] = useState('');
 
@@ -28,12 +36,7 @@ const UserSettings = () => {
     const [insuranceNumber, setInsuranceNumber] = useState('');
 
 
-    const handlePictureClick = (e) => {
-        e.preventDefault();
-        console.log(session?.user?.image);
-    };
-
-    const handleSubmit = (e) => {   
+    const handleSubmit = (e) => {
         e.preventDefault();
         console.log(role);
     };
@@ -46,80 +49,94 @@ const UserSettings = () => {
                 <div className="absolute inset-0 bg-gray-900 opacity-50"></div>
             </div>
             {/* Profile picture with edit icon */}
-            <div
-                className="relative mx-auto -mt-16 w-32 h-32 rounded-full overflow-hidden border-4 border-white cursor-pointer"
-                onClick={handlePictureClick}
+            <div className="relative mx-auto -mt-16 w-32 h-32 rounded-full overflow-hidden border-4 border-white hover:cursor-pointer"
             >
-            <img
-                src={profileImage ? profileImage : "https://avatars.githubusercontent.com/u/72605819?v=4"}
-                alt={session?.user?.name}
-                className="w-full h-full object-cover"
-                />
+
+                <label htmlFor="profileImageInput" className="w-full h-full">
+                    {profileImage ? (
+                        <img
+                            src={profileImage}
+                            alt={session?.user?.name}
+                            className="w-full h-full object-cover"
+                        />
+                    ) : (
+                        <span className="flex items-center justify-center w-full h-full">
+                            Upload Picture
+                        </span>
+                    )}
+                    <input
+                        id="profileImageInput"
+                        type="file"
+                        accept="image/*"
+                        className="hidden"
+                        onChange={handlePictureChange}
+                    />
+                </label>
             </div>
             <div className="mx-auto max-w-screen-lg my-8 px-4">
                 <form onSubmit={handleSubmit}>
                     <TextInput
                         label="Name*"
-                        value={idNumber}
-                        onChange={(e) => setIdNumber(e.target.value)}
+                        value={firstName}
+                        onChange={(e) => setFirstName(e.target.value)}
                     />
                     <TextInput
                         label="Surname*"
-                        value={idNumber}
-                        onChange={(e) => setIdNumber(e.target.value)}
+                        value={lastName}
+                        onChange={(e) => setLastName(e.target.value)}
                     />
 
                     <CustomButton
-                            buttonText={"Save Changes"}
-                            onClick={() => {
-                                //Change name and username
-                            }}
-                        />
+                        buttonText={"Save Changes"}
+                        onClick={() => {
+                            //Change name and username
+                        }}
+                    />
                     <TextInput
                         label="Email*"
-                        value={idNumber}
+                        value={email}
                         onChange={(e) => setEmail(e.target.value)}
                     />
                     <CustomButton
-                            buttonText={"Save Changes"}
-                            onClick={() => {
-                                //Change name and username
-                            }}
-                        />
-
-                        
-                    {role == Role.PATIENT && (
-                    // <div>
-                    // <InsuranceForm
-                    //     hasInsurance={hasInsurance}
-                    //     setHasInsurance={setHasInsurance}
-                    //     insuranceNumber={insuranceNumber}
-                    //     setInsuranceNumber={setInsuranceNumber}
-                    // />
-
-                    //       <CustomButton
-                    //         buttonText={"Save Changes"}
-                    //         onClick={() => {
-                    //             //Change name and username
-                    //         }}
-                    //     />
-                    // </div>
-                    <div>
-                    <TextInput
-                        label="Innsurance Number*"
-                        value={insuranceNumber}
-                        onChange={(e) => setInsuranceNumber(e.target.value)}
+                        buttonText={"Save Changes"}
+                        onClick={() => {
+                            //Change name and username
+                        }}
                     />
-                        <CustomButton
-                            buttonText={"Save Changes"}
-                            onClick={() => {
-                                //Change the insurance number
-                            }}
-                        />
-                    </div>
+
+
+                    {role == Role.PATIENT && (
+                        // <div>
+                        // <InsuranceForm
+                        //     hasInsurance={hasInsurance}
+                        //     setHasInsurance={setHasInsurance}
+                        //     insuranceNumber={insuranceNumber}
+                        //     setInsuranceNumber={setInsuranceNumber}
+                        // />
+
+                        //       <CustomButton
+                        //         buttonText={"Save Changes"}
+                        //         onClick={() => {
+                        //             //Change name and username
+                        //         }}
+                        //     />
+                        // </div>
+                        <div>
+                            <TextInput
+                                label="Innsurance Number*"
+                                value={insuranceNumber}
+                                onChange={(e) => setInsuranceNumber(e.target.value)}
+                            />
+                            <CustomButton
+                                buttonText={"Save Changes"}
+                                onClick={() => {
+                                    //Change the insurance number
+                                }}
+                            />
+                        </div>
                     )}
 
-           
+
                 </form>
             </div>
         </div>
