653de85bcdbaa54f750111d6c2fa63f86e7fe6fe
Michal <smaluchmichal@gmail.com>
2023-05-25 21:53:11 +0200

    link to settings when we click on profile pic

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,134 +5,135 @@ import { Role } from "@prisma/client";
 import ProfilePicture from "@/components/ProfilePicture";
 
 export default function sideBar() {
-  const { data: session } = useSession();
+    const { data: session } = useSession();
 
-  let menuItems = [
-    {
-      href: "/about",
-      title: "About",
-    },
-    {
-      href: "/contact",
-      title: "Contact",
-    },
-    {
-      href: "/locations",
-      title: "Find Us",
-    },
-  ];
+    let menuItems = [
+        {
+            href: "/about",
+            title: "About",
+        },
+        {
+            href: "/contact",
+            title: "Contact",
+        },
+        {
+            href: "/locations",
+            title: "Find Us",
+        },
+    ];
 
-  let path = "/";
+    let path = "/";
 
-  if (session?.user?.role === Role.PATIENT) {
-    path = "/patient";
-    menuItems = menuItems
-      .concat([
-        {
-          href: "/tests",
-          title: "Tests",
-        },
-        {
-          href: "/visits",
-          title: "Appointments",
-        },
-        {
-          href: "/profile/messages",
-          title: "Messages",
-        },
-      ])
-      .reverse();
-  } else if (session?.user?.role === Role.DOCTOR) {
-    path = "/doctor";
-    menuItems = menuItems
-      .concat([
-        {
-          href: "/tests",
-          title: "Tests",
-        },
-        {
-          href: "/visits",
-          title: "Appointments",
-        },
-      ])
-      .reverse();
-  } else if (session?.user?.role === Role.LAB_ASSISTANT) {
-    menuItems = menuItems
-      .concat([
-        {
-          href: "/tests",
-          title: "My Tests",
-        },
-        {
-          href: "/all-tests",
-          title: "Tests",
-        },
-      ])
-      .reverse();
-  } else if (session?.user?.role === Role.LAB_SUPERVISOR) {
-    menuItems = menuItems
-      .concat([
-        {
-          href: "/tests",
-          title: "My Tests",
-        },
-        {
-          href: "/all-tests",
-          title: "Tests",
-        },
-      ])
-      .reverse();
-  } else if (session?.user?.role === Role.RECEPTIONIST) {
-  } else {
-    // menuItems = [
-    //     {
-    //         href: "/about",
-    //         title: "About",
-    //     },
-    //     {
-    //         href: "/contact",
-    //         title: "Contact",
-    //     },
-    //     {
-    //         href: "/locations",
-    //         title: "Find Us",
-    //     },
-    // ];
-  }
+    if (session?.user?.role === Role.PATIENT) {
+        path = "/patient";
+        menuItems = menuItems
+            .concat([
+                {
+                    href: "/tests",
+                    title: "Tests",
+                },
+                {
+                    href: "/visits",
+                    title: "Appointments",
+                },
+                {
+                    href: "/profile/messages",
+                    title: "Messages",
+                },
+            ])
+            .reverse();
+    } else if (session?.user?.role === Role.DOCTOR) {
+        path = "/doctor";
+        menuItems = menuItems
+            .concat([
+                {
+                    href: "/tests",
+                    title: "Tests",
+                },
+                {
+                    href: "/visits",
+                    title: "Appointments",
+                },
+            ])
+            .reverse();
+    } else if (session?.user?.role === Role.LAB_ASSISTANT) {
+        menuItems = menuItems
+            .concat([
+                {
+                    href: "/tests",
+                    title: "My Tests",
+                },
+                {
+                    href: "/all-tests",
+                    title: "Tests",
+                },
+            ])
+            .reverse();
+    } else if (session?.user?.role === Role.LAB_SUPERVISOR) {
+        menuItems = menuItems
+            .concat([
+                {
+                    href: "/tests",
+                    title: "My Tests",
+                },
+                {
+                    href: "/all-tests",
+                    title: "Tests",
+                },
+            ])
+            .reverse();
+    } else if (session?.user?.role === Role.RECEPTIONIST) {
+    } else {
+        // menuItems = [
+        //     {
+        //         href: "/about",
+        //         title: "About",
+        //     },
+        //     {
+        //         href: "/contact",
+        //         title: "Contact",
+        //     },
+        //     {
+        //         href: "/locations",
+        //         title: "Find Us",
+        //     },
+        // ];
+    }
 
-  return (
-    <aside className="bg-gray-100 w-full md:w-60">
-      {session ? (
-        <div className="flex items-center py-4 px-4 flex-col">
-          <ProfilePicture
-            src={session.user?.image ? session.user?.image : "https://t3.ftcdn.net/jpg/00/64/67/80/360_F_64678017_zUpiZFjj04cnLri7oADnyMH0XBYyQghG.jpg"}
-            alt={session?.user?.name}
-            size={5}
-          />
-          <div>
-            <h2 className="text-xs font-bold">{session?.user?.name}</h2>
-            {/* <p className="text-xs text-gray-500">{session?.user?.email}</p> */}
-          </div>
-        </div>
-      ) : null}
-      <nav>
-        <ul>
-          {menuItems.map(({ href, title }) => (
-            <li className="m-2" key={title}>
-              <Link href={href} passHref>
-                <div
-                  className="flex p-2 bg-gray-50 hover:bg-gray-200 cursor-pointer rounded-lg
-                     "
-                >
-                  <span className="font-semibold text-gray-600 mx-6 text-sm">
-                    {title}
-                  </span>
+    return (
+        <aside className="bg-gray-100 w-full md:w-60">
+            {session ? (
+                <div className="flex items-center py-4 px-4 flex-col">
+                    <Link href={"/settings"}>
+                        <ProfilePicture
+                            src={session.user?.image ? session.user?.image : "https://t3.ftcdn.net/jpg/00/64/67/80/360_F_64678017_zUpiZFjj04cnLri7oADnyMH0XBYyQghG.jpg"}
+                            alt={session?.user?.name}
+                            size={5}
+                        />
+                    </Link>
+                    <div>
+                        <h2 className="text-xs font-bold">{session?.user?.name}</h2>
+                    </div>
                 </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </aside>
-  );
+            ) : null}
+            <nav>
+                <ul>
+                    {menuItems.map(({ href, title }) => (
+                        <li className="m-2" key={title}>
+                            <Link href={href} passHref>
+                                <div
+                                    className="flex p-2 bg-gray-50 hover:bg-gray-200 cursor-pointer rounded-lg
+                     "
+                                >
+                                    <span className="font-semibold text-gray-600 mx-6 text-sm">
+                                        {title}
+                                    </span>
+                                </div>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </aside>
+    );
 }
