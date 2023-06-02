26ca61bbe26ff8231f60370639a321590d36e23d
Asser Elfeki <assemou414@student.polsl.pl>
2023-06-02 18:49:09 +0200

    chore: add Prettier as default formatter for JavaScript files feat(visit.tsx): add conditional rendering for different user roles and visit statuses to display relevant information and buttons

--- src/pages/visits/[id].tsx
+++ src/pages/visits/[id].tsx
@@ -9,68 +9,73 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 export default function Visit() {
+    const { data: session } = useSession()
 
     const [visit, setVisit] = useState({})
     const [tests, setTests] = useState([])
 
     const router = useRouter()
     const { id } = router.query
-    const { data: session } = useSession()
 
     const fetchData = async () => {
-        if (session?.user) {
+        if (session?.user && id !== undefined) {
             let res = await getVisitDetails(session.user?.role, id)
             setVisit(res['data'])
-            res = await getTestsOfAVisit(session.user?.role, id)
-            setTests(res['data'])
+            if (res['data']['status'] != Status.REGISTERED) {
+                res = await getTestsOfAVisit(session.user?.role, id)
+                setTests(res['data'])
+            }
         }
     }
 
     useEffect(() => {
         fetchData()
-    }, [session])
+    }, [session, id])
 
     return (
         <div className="mx-auto max-w-screen-lg my-8 px-4">
             <h1 className="text-3xl font-bold mb-6">Visit details</h1>
-            {visit['description'] &&
-                <div className="w-full md:w-1/2 px-4 mb-4">
-                    <Label name="Visit description" value={visit['description']} />
+
+            {(visit['status'] == Status.REGISTERED && session.user?.role === Role.DOCTOR) && // for doctor before the visit
+                <div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                        <Label name="Visit date" value={dayjs(visit['date']).toString()} />
+                    </div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                        <Label name="Visit status" value={visit['status']} />
+                    </div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                        <Label name="Visit description" value={visit['description']} />
+                    </div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                        <Label name="Patient" value={visit['patient']['user']['firstName'] + " " + visit['patient']['user']['lastName']} />
+                    </div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                        <CustomButton
+                            buttonText={"Cancel visit"}
+                            onClick={() => {
+                            router.push("#");
+                            }}
+                        />
+                    </div>
                 </div>
             }
 
-            {visit['diagnosis'] &&
-                <div className="w-full md:w-1/2 px-4 mb-4">
-                    <Label name="Visit diagnosis" value={visit['diagnosis']} />
-                </div>
-            }
-            {visit['status'] &&
-                <div className="w-full md:w-1/2 px-4 mb-4">
-                    <Label name="Visit status" value={visit['status']} />
-                </div>
-            }
-            {visit['date'] &&
-                <div className="w-full md:w-1/2 px-4 mb-4">
+            {(visit['status'] == Status.REGISTERED && session.user?.role === Role.PATIENT) && // for patient before the visit 
+                <div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
                     <Label name="Visit date" value={dayjs(visit['date']).toString()} />
                 </div>
-            }
-            {session?.user?.role == Role.PATIENT && visit['doctorId'] &&
-                <div className="w-full md:w-1/2 px-4 mb-4">
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                    <Label name="Visit status" value={visit['status']} />
+                    </div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
                     <Label name="Doctor" value={visit['doctor']['user']['firstName'] + " " + visit['doctor']['user']['lastName']} />
                 </div>
-            }
-            {session?.user?.role == Role.DOCTOR && visit['patientId'] &&
-                <div className="w-full md:w-1/2 px-4 mb-4">
-                    <Label name="Patient" value={visit['patient']['user']['firstName'] + " " + visit['patient']['user']['lastName']} />
-                </div>
-            }
-            {tests != null &&
-                <div className="w-full md:w-1/2 px-4 mb-4">
-                     <Label name="Tests" value={tests?.map((test)=> test['doctorNote'])} />
-                </div>
-            }
-            {visit['status'] == Status.REGISTERED &&
-                <div className="w-full md:w-1/2 px-4 mb-4">
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                        <Label name="Visit description" value={visit['description']} />
+                    </div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
                     <CustomButton
                         buttonText={"Cancel visit"}
                         onClick={() => {
@@ -78,7 +83,53 @@ export default function Visit() {
                         }}
                     />
                 </div>
+                </div>
             }
+
+            {(visit['status'] == Status.COMPLETED && session.user?.role === Role.PATIENT) && // for patient after the visit
+                <div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                    <Label name="Visit date" value={dayjs(visit['date']).toString()} />
+                </div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                    <Label name="Visit status" value={visit['status']} />
+                    </div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                    <Label name="Doctor" value={visit['doctor']['user']['firstName'] + " " + visit['doctor']['user']['lastName']} />
+                </div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                        <Label name="Visit description" value={visit['description']} />
+                    </div>
+
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                        <Label name="Visit diagnosis" value={visit['diagnosis']} />
+                    </div>
+                </div>
+            }
+
+            {(visit['status'] != Status.REGISTERED && session.user?.role === Role.DOCTOR) && //for doctor during and after the visit 
+                <div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                        <Label name="Visit date" value={dayjs(visit['date']).toString()} />
+                    </div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                        <Label name="Visit status" value={visit['status']} />
+                    </div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                        <Label name="Visit description" value={visit['description']} />
+                    </div>
+
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                        <Label name="Visit diagnosis" value={visit['diagnosis']} />
+                    </div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                        <Label name="Patient" value={visit['patient']['user']['firstName'] + " " + visit['patient']['user']['lastName']} />
+                    </div>
+                    <div className="w-full md:w-1/2 px-4 mb-4">
+                        <Label name="Tests" value={tests.map((test)=> test['doctorNote'])} />
+                    </div>
+                </div>
+            } 
         </div>
     );
 }
