662b0fa9f857beffdef4d20562f3eddb8d05684a
youssefalb <yousalb891@student.polsl.pl>
2023-06-08 20:06:54 +0200

    improved the design od usercards

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -14,33 +14,36 @@ const UserCard = ({ id, name, surname, role, nationalID, isActive }) => {
       <div className="bg-white hover:bg-gray-100 rounded-lg shadow-md p-6 flex items-center justify-between">
         <div className="flex items-center">
           <div>
-            <span className="text-gray-80 text-sm font-semibold">{name} {surname} ({role})</span>
+            <span className="text-gray-80 text-sm font-semibold">{name} {surname} </span>
           </div>
         </div>
+        <div className="relative rounded-full text-white p-5 bg-gray-500 font-semibold w-30 h-10 flex items-center justify-center">
+          <span className="text-sm">{role}</span>
+        </div>
+        <div>
+          <span className="text-gray-80 text-sm font-normal">{nationalID} </span>
+        </div>
+        {activeState ? (
+          <span className="ml-2">
+            <img
+              src="/images/active-user.png"
+              alt="Ban Icon"
+              className="h-10 w-10"
+              title="Ban User"
+            />
+          </span>
+        ) : (
+          <span className="ml-2">
+            <img
+              src="/images/banned-user.png"
+              alt="Unban Icon"
+              className="h-10 w-10"
+              title="Unban User"
+            />
+          </span>
+        )}
+      </div>
         <div className="flex items-center">
-          <div className="relative rounded-full text-white p-5 bg-blue-500 font-semibold w-30 h-10 flex items-center justify-center">
-            <span className="text-sm">ID: {nationalID}</span>
-          </div>
-          {activeState ? (
-            <span className="ml-2">
-              <img
-                src="/images/active-user.png"
-                alt="Ban Icon"
-                className="h-10 w-10"
-                title="Ban User"
-              />
-            </span>
-          ) : (
-            <span className="ml-2">
-              <img
-                src="/images/banned-user.png"
-                alt="Unban Icon"
-                className="h-10 w-10"
-                title="Unban User"
-              />
-            </span>
-          )}
-        </div>
       </div>
 
     </div>
