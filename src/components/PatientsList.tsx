953feb41d8fba365f775bd0b4af7cf5c44ff7b55
youssefalb <yousalb891@student.polsl.pl>
2023-06-08 17:22:19 +0200

    search for patients is possible now

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -7,11 +7,13 @@ import { ToastContainer } from "react-toastify";
 import EmptyStateMessage from "./EmptyStateMessage";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import { TextField } from "@mui/material";
 
 export default function PatientsList() {
   const { data: session } = useSession(); // it's not fired everytime, (only once), but I need to declare it to be able to access it
   const [isLoading, setIsLoading] = useState(true);
   const [patients, setPatients] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
   const router = useRouter();
 
   const fetchData = async () => {
@@ -27,11 +29,29 @@ export default function PatientsList() {
     // else loading state. show loading state
   }, [session]);
 
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredPatients = patients.filter((patient) =>
+    `${patient.user.firstName} ${patient.user.lastName} ${patient.user.nationalId}`
+      .toLowerCase()
+      .includes(searchQuery.toLowerCase())
+  );
+
   return (
+    <div>
+        <TextField
+          type="text"
+          placeholder="Search patients..."
+          value={searchQuery}
+          onChange={handleSearch}
+          className="w-full p-2 border border-gray-300 rounded"
+        />
     <div className="flex flex-col-reverse gap-4">
-      <ToastContainer />
-      {patients?.length ? (
-        patients.map((patient) => (
+
+      {filteredPatients?.length ? (
+        filteredPatients.map((patient) => (
           <Link key={patient.patientId} href={`user-settings/${patient.patientId}`} >
             <UserCard
               id={patient.patientId}
@@ -50,6 +70,7 @@ export default function PatientsList() {
         />
       )}
 
+    </div>
     </div>
   );
 }
